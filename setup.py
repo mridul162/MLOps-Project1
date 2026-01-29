78a590b404ddc32e616899b203d799c095e5f413
mridul162 <mridulbd75@gmail.com>
2026-01-29 11:56:37 +0600

    setup file updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    author="Vikash Das",
-    author_email="vikashdas770@gmail.com",
+    author="Asifur Rahman Mridul",
+    author_email="mridulbd75@gmail.com",
     packages=find_packages()
 )
